753aa5e87290a5043bb489f0b00ae83465f5a037
Mert Can Altın <mertgold60@gmail.com>
2023-01-11 16:09:09 +0300

    dev: added redux toolkit & created store

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,10 @@
+import store from "../store/index";
+import { Provider } from "react-redux";
 import '../styles/globals.css'
 
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return <Provider store={store}>
+    <Component {...pageProps} />
+  </Provider>
+
 }
