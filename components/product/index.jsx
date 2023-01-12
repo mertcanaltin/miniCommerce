e2c101298ce0af24488c452a3a4fbdceb283b03a
Mert Can Altın <mertgold60@gmail.com>
2023-01-12 13:49:04 +0300

    created productItem component

--- components/product/index.jsx
+++ components/product/index.jsx
@@ -1,7 +1,29 @@
 import React from 'react'
+import Image from 'next/image';
 
-export default function product() {
+export default function ProductItem({ productId, productImage, productPrice, productName, productHref }) {
   return (
-    <div>product</div>
+    <div key={productId} className="group relative">
+      <div className="min-h-80 aspect-w-1 aspect-h-1 w-full overflow-hidden rounded-md bg-gray-200 group-hover:opacity-75 lg:aspect-none lg:h-80">
+        <Image
+          className="h-full w-full object-cover object-center lg:h-full lg:w-full"
+          src={productImage}
+          alt="tefal"
+          width={500}
+          height={500}
+        />
+      </div>
+      <div className="mt-4 flex justify-between">
+        <div>
+          <h3 className="text-sm text-gray-700">
+            <a href={productHref}>
+              <span aria-hidden="true" className="absolute inset-0" />
+              {productName}
+            </a>
+          </h3>
+        </div>
+        <p className="text-sm font-medium text-gray-900">{productPrice}</p>
+      </div>
+    </div>
   )
 }
--- pages/list/index.jsx
+++ pages/list/index.jsx
@@ -1,5 +1,6 @@
 import React, { useState, useEffect } from 'react'
-import Image from 'next/image';
+import ProductItem from '../../components/product';
+
 
 export function getProducts() {
   return fetch('/api/getProducts')
@@ -28,29 +29,7 @@ export default function List() {
 
         <div className="mt-6 grid grid-cols-1 gap-y-10 gap-x-6 sm:grid-cols-2 lg:grid-cols-4 xl:gap-x-8">
           {products.map((product) => (
-            <div key={product.id} className="group relative">
-              <div className="min-h-80 aspect-w-1 aspect-h-1 w-full overflow-hidden rounded-md bg-gray-200 group-hover:opacity-75 lg:aspect-none lg:h-80">
-                <Image
-                  className="h-full w-full object-cover object-center lg:h-full lg:w-full"
-                  src={product.productimage_set[0].image}
-                  alt="Picture of the author"
-                  width={500}
-                  height={500}
-                />
-              </div>
-              <div className="mt-4 flex justify-between">
-                <div>
-                  <h3 className="text-sm text-gray-700">
-                    <a href={product.href}>
-                      <span aria-hidden="true" className="absolute inset-0" />
-                      {product.name}
-                    </a>
-                  </h3>
-                  <p className="mt-1 text-sm text-gray-500">{product.color}</p>
-                </div>
-                <p className="text-sm font-medium text-gray-900">{product.price}</p>
-              </div>
-            </div>
+            <ProductItem productId={product.id} productImage={product.productimage_set[0].image} productName={product.name}/>
           ))}
         </div>
       </div>
