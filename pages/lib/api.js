d763ed38ff74a5abe7f2d864464bca2a87c12da7
Mert Can Altın <mertgold60@gmail.com>
2023-01-16 19:45:34 +0300

    dev : created filter & delete dummy fake api

--- pages/lib/api.js
+++ pages/lib/api.js
@@ -1,26 +1,25 @@
 export async function fetchProducts() {
-  const res = await fetch(`${process.env.NEXT_PUBLIC_VERCEL_URL}/api/getProducts`)
+  const res = await fetch(`https://shop.samsung.com/tr/list/?format=json`)
   const data = await res.json()
 
   return data
 }
 
 export async function getAllProductPks() {
-  const res = await fetch(`${process.env.NEXT_PUBLIC_VERCEL_URL}/api/getProducts`);
+  const res = await fetch(`https://shop.samsung.com/tr/list/?format=json`);
   const { products } = await res.json();
 
   return products.map(product => {
     return {
       params: {
         pk: product.pk.toString()
-
       }
     }
   })
 }
 
 export async function getProductData(pk) {
-  const res = await fetch(`${process.env.NEXT_PUBLIC_VERCEL_URL}/api/getProducts`);
+  const res = await fetch(`https://shop.samsung.com/tr/list/?format=json`);
   const { products } = await res.json();
   const product = products.find(product => product.pk == pk)
 
--- pages/list/index.jsx
+++ pages/list/index.jsx
@@ -2,10 +2,11 @@ import React, { useState, useEffect } from 'react'
 import { fetchProducts } from '../lib/api';
 import ProductItem from '../../components/product';
 import Pagination from '../../components/pagination';
-import { paginate } from '../../components/pagination/helpers/paginate'
+import Filter from '../../components/filter';
+import { paginate } from '../../components/pagination/helpers/paginate'
 
 
-export default function List({ data }) {
+export default function List({ products,facets }) {
 
   const [currentPage, setCurrentPage] = useState(1);
   const pageSize = 20;
@@ -14,39 +15,44 @@ export default function List({ data }) {
     setCurrentPage(page);
   };
 
-  const paginatedPosts = paginate(data, currentPage, pageSize);
+  const paginatedPosts = paginate(products, currentPage, pageSize);
 
   return (
-    <div className="bg-white">
-      <div className="mx-auto max-w-2xl py-16 px-4 sm:py-24 sm:px-6 lg:max-w-7xl lg:px-8">
-        <h2 className="text-2xl font-bold tracking-tight text-gray-900">List</h2>
-
-        <div className="mt-6 grid grid-cols-1 gap-y-10 gap-x-6 sm:grid-cols-2 lg:grid-cols-4 xl:gap-x-8">
-          {paginatedPosts.map((product) => (
-              <ProductItem  key={product.pk} data-pk={product.pk} product={product}/>
-          ))}
+    <>
+      <div className="bg-white flex">
+        <Filter facets={facets} />
+        <div className="mx-auto max-w-2xl py-16 px-4 sm:py-24 sm:px-6 lg:max-w-7xl lg:px-8">
+          <h2 className="text-2xl font-bold tracking-tight text-gray-900">List</h2>
+          <div className="mt-6 grid grid-cols-1 gap-y-10 gap-x-6 sm:grid-cols-2 lg:grid-cols-4 xl:gap-x-8">
+            {paginatedPosts.map((product) => (
+              <ProductItem key={product.pk} data-pk={product.pk} product={product} />
+            ))}
+          </div>
         </div>
       </div>
 
       <Pagination
-       items={data.length}
-       currentPage={currentPage}
-       pageSize={pageSize}
-       onPageChange={onPageChange}
-        />
-    </div>
+        items={products.length}
+        currentPage={currentPage}
+        pageSize={pageSize}
+        onPageChange={onPageChange}
+      />
+    </>
   )
 }
 
 
 export const getStaticProps = async () => {
 
-  let data;
+  let products, facets;
   await fetchProducts().then(response => {
-    data = response.products
+    
+    products = response.products
+    facets = response.facets
+
   });
- 
+
   return {
-    props: { data },
+    props: { products,facets },
   };
- };
+};
